4fce1f0d0721f95a4ce628f5bb9c1242e782449a
tstuart <tstuart@beckshybrids.com>
2015-05-29 15:44:43 -0400

    add socket io to server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 
 
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.listen(8080);
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+});
+
+server.listen(8080);
 
