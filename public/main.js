80f1db1ca786b120a244c39dac839bf0098db8a7
tstuart <tstuart@beckshybrids.com>
2015-05-30 11:34:37 -0400

    finished enhanced chat

--- public/main.js
+++ public/main.js
@@ -8,15 +8,27 @@ $(document).ready(function() {
         nickName = window.prompt("Please enter your nickname:", "");
     }
 
+    $('#nickName').html(nickName);
+
     var socket = io();
     socket.emit('regUser', nickName);
 
     var input = $('input');
     var messages = $('#messages');
     var clientCount = $('#clientCount');
+    var typing = $('#userTyping');
 
     // add message to message area
-    var addMessage = function(message) {
+    var addMessage = function(message, includeNickName) {
+
+        // look for pm|{username}|{message} format
+        var args = message.split('|');
+        if (args.length === 3) {
+            message = args[2];
+        }
+        if (includeNickName) {
+            message = nickName + ': ' + message;
+        }
         messages.append('<div>' + message + '</div>');
     };
 
@@ -34,18 +46,42 @@ $(document).ready(function() {
         $("ul").html(html);
     };
 
+    // update {username} is typing div
+    var userTyping = function(nickName) {
+        typing.html(nickName + ' is typing.');
+    };
 
+    // clear {username} is typing div
+    var clearUserTyping = function() {
+        typing.html('');
+    };
+
+    // socket event handlers
     socket.on('message', addMessage);
     socket.on('clientsChanged', updClients);
+    socket.on('keydown', userTyping);
+    socket.on('keyup', clearUserTyping);
 
+    // on keydown, if enter, send message.
+    // otherwise emit 'keydown' event
     input.on('keydown', function(event) {
         if (event.keyCode != 13) {
+            console.log('keydown');
+            socket.emit('keydown');
             return;
         }
 
         var message = input.val();
-        addMessage(message);
+        addMessage(message, true);
         socket.emit('message', message);
         input.val('');
     });
+
+    // on keyup, emit 'keyup' event if not enter
+    input.on('keyup', function(event) {
+        if (event.keyCode != 13) {
+            socket.emit('keyup');
+        }
+    });
+
 });
--- server.js
+++ server.js
@@ -13,10 +13,26 @@ var io = socket_io(server);
 // use socket id, and nickName
 var clients = new Object();
 
+// get client id by nickname
+var getClientID = function(nickName) {
+    var id = null;
+    for(var index in clients) {
+        if (clients.hasOwnProperty(index)) {
+            if (clients[index] === nickName) {
+                id = index;
+                break;
+            }
+        }
+    }
+    return id;
+};
+
 io.on('connection', function (socket) {
 
-    // do whatever when a client connects
-
+    // to handle nick name support, clients will
+    // emit 'regUser' event after getting nick name
+    // from user.  This handler will add client to
+    // our list of clients.
     socket.on('regUser', function(nickName) {
         clients[socket.id] = nickName;
         socket.broadcast.emit('message', nickName + ' has connected.');
@@ -24,17 +40,56 @@ io.on('connection', function (socket) {
 
     });
 
+    // This will execute any time a client sends a message
     socket.on('message', function(message) {
-        console.log('Received Message:', message);
-        socket.broadcast.emit('message', message);
+
+        // get nickName of sender
+        var nickNameSender = clients[socket.id];
+
+        // determine if this is a private message
+        // it is a private message if the message
+        // is in pm|{nickname}|{message} format
+        var args = message.split('|');
+        if (args.length === 3 && args[0] === 'pm') {
+            // get socket associated with nickname
+            // if there is no associated socket,
+            // server will ignore message.
+            var socketID = getClientID(args[1]);
+            if (socketID) {
+                io.sockets.connected[socketID].emit('message', nickNameSender + ': ' + args[2]);
+            }
+        } else {
+            // this is not a private message, emit as normal
+            socket.broadcast.emit('message', nickNameSender + ': ' + message);
+        }
     });
 
+    // this will execute when a client disconnects
     socket.on('disconnect', function() {
+        if (!clients[socket.id]) {
+            return;
+        }
         var nickName = clients[socket.id];
         delete clients[socket.id];
         socket.broadcast.emit('message', nickName + ' has disconnected.');
         io.emit('clientsChanged', clients);
     });
+
+    // this will execute when a client socket emits a keydown event
+    // simply emit this event back to other clients.  This will be
+    // use to add {user} is typing functionality
+    socket.on('keydown', function() {
+        var nickName = clients[socket.id];
+        socket.broadcast.emit('keydown', nickName);
+    });
+
+    // this will execute when a client socket emits a keyup event
+    // simply emit this event back to other clients.  This will be
+    // use to add {user} is typing functionality
+    socket.on('keyup', function() {
+        socket.broadcast.emit('keyup');
+    });
+
 });
 
 server.listen(8080);
