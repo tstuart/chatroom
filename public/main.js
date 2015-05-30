9e441b68282975a67d33ac3fe796100ec9b67db5
tstuart <tstuart@beckshybrids.com>
2015-05-30 07:51:00 -0400

    added nicknames, client count, client list.

--- public/main.js
+++ public/main.js
@@ -1,15 +1,42 @@
 
 
 $(document).ready(function() {
+
+    // prompt for nickname and set to socket
+    var nickName = null;
+    while (!nickName) {
+        nickName = window.prompt("Please enter your nickname:", "");
+    }
+
     var socket = io();
+    socket.emit('regUser', nickName);
+
     var input = $('input');
     var messages = $('#messages');
+    var clientCount = $('#clientCount');
 
+    // add message to message area
     var addMessage = function(message) {
         messages.append('<div>' + message + '</div>');
     };
 
+    // update client count
+    var updClients = function(clients) {
+        clientCount.html('Connected Clients = ' + Object.keys(clients).length);
+
+        $("ul").empty();
+        var html = "";
+        for(var index in clients) {
+            if (clients.hasOwnProperty(index)) {
+                html += "<li>" + clients[index] + "</li>";
+            }
+        }
+        $("ul").html(html);
+    };
+
+
     socket.on('message', addMessage);
+    socket.on('clientsChanged', updClients);
 
     input.on('keydown', function(event) {
         if (event.keyCode != 13) {
--- server.js
+++ server.js
@@ -9,12 +9,31 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+// keep copy of connected clients
+// use socket id, and nickName
+var clients = new Object();
+
 io.on('connection', function (socket) {
-    console.log('Client connected');
+
+    // do whatever when a client connects
+
+    socket.on('regUser', function(nickName) {
+        clients[socket.id] = nickName;
+        socket.broadcast.emit('message', nickName + ' has connected.');
+        io.emit('clientsChanged', clients);
+
+    });
 
     socket.on('message', function(message) {
         console.log('Received Message:', message);
         socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', function() {
+        var nickName = clients[socket.id];
+        delete clients[socket.id];
+        socket.broadcast.emit('message', nickName + ' has disconnected.');
+        io.emit('clientsChanged', clients);
     });
 });
 
